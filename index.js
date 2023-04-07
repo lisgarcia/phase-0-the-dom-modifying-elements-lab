f9ec0e5816ec1dfedd5c2f0eeba10550912709c5
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-06 21:46:16 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+const newHeader=document.createElement("h1");
+newHeader.setAttribute('id', 'victory');
+newHeader.id='victory'
+newHeader.innerHTML="Lis is the champion";
+document.body.append(newHeader);
+
+
+
