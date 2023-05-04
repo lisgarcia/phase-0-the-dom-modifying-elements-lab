707dbcd4a48c4cf19cd14cb0257ad12bb2a991f9
Elisabeth Garcia <lisgarciae@gmail.com>
2023-05-04 19:55:05 -0400

    done again

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-main.remove();
-const newHeader=document.createElement("h1");
-newHeader.setAttribute('id', 'victory');
-newHeader.id='victory'
-newHeader.innerHTML="Lis is the champion";
-document.body.append(newHeader);
+const newHeader = document.querySelector("h1")
+    newHeader.setAttribute('id', "victory")
+    newHeader.textContent = 'Lis is the champion'
+
+
+// main.remove();
+// const newHeader=document.createElement("h1");
+// newHeader.setAttribute('id', 'victory');
+// newHeader.id='victory'
+// newHeader.innerHTML="Lis is the champion";
+// document.body.append(newHeader);
 
 
 
